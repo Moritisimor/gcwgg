d84a78a8cfc1f4098cb8bc119cbec449bce2d93a
Moritisimor <devMoritisimor@proton.me>
2026-04-08 01:54:17 +0200

    Implemented support for reading words from files

--- cmd/gcwgg/main.go
+++ cmd/gcwgg/main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"math/rand"
+	"os"
 
 	"github.com/Moritisimor/EpsilonFetch/pkg/color"
 	"github.com/Moritisimor/gcwgg/internal"
 )
 
 func main() {
-	words, err := internal.ReadFileToLines("words.txt")
+	if len(os.Args) < 2 {
+		color.PrintRedln("Usage: gcwgg <file>")
+		return
+	}
+
+	words, err := internal.ReadFileToLines(os.Args[1])
 	if err != nil {
 		color.PrintRedln("Error while reading file: " + err.Error())
 		return
