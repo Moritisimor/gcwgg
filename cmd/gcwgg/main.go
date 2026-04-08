ca2095d4a1c9263385857ecb0d113122a7c7be26
Moritisimor <devMoritisimor@proton.me>
2026-04-08 05:03:23 +0200

    Fixed bug where not supplying a CLI arg would crash the entire thing

--- cmd/gcwgg/main.go
+++ cmd/gcwgg/main.go
@@ -10,15 +10,17 @@ import (
 
 func main() {
 	var words []string
+	var err error
+
 	if len(os.Args) < 2 {
 		words = internal.GetFallbackData()
-	}
-
-	words, err := internal.ReadFileToLines(os.Args[1])
-	if err != nil {
-		color.PrintRedln("Error while reading file: " + err.Error())
-		color.PrintRedln("Using fallback words instead.")
-		words = internal.GetFallbackData()
+	} else {
+		words, err = internal.ReadFileToLines(os.Args[1])
+		if err != nil {
+			color.PrintRedln("Error while reading file: " + err.Error())
+			color.PrintRedln("Using fallback words instead.")
+			words = internal.GetFallbackData()
+		}
 	}
 
 	internal.GameLoop(words[rand.Int31n(int32(len(words) - 1))])
